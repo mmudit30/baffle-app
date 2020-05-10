529d9cee1c121fce294989720d7ff1cd03b00ec4
Mudit Mittal <mmudit30@gmail.com>
2020-05-10 14:52:05 +0530

    changes and build

--- Angular/server.js
+++ Angular/server.js
@@ -21,10 +21,10 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/build'));
 
 app.get('/*', (req,res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 3000)
--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -9,7 +9,6 @@ const routes: Routes = [
   {
     path: '', pathMatch: 'full', 
     redirectTo:'home'
-    // redirectTo:'calendar'
   },
   {
     path: 'home', component: HomeComponent
